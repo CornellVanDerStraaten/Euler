c542643902892ad78459390e4dc14d1c5d746766
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-01-05 22:02:06 +0100

    Completed Problem 2, first try

--- 2/script.php
+++ 2/script.php
@@ -1,9 +1,29 @@
 <?php
 
 /*
- * NOT FINISHED
+ * FINISHED
+ *
+ * ANSWER: 4613732
  *
  * STARTED: 5 JANUARI 2022
+ * FINISHED: 5 JANUARI 2022
  */
 
+$maximum = 4000000;
+$sum = 0;
 
+$previousValue = 1;
+$currentValue = 1;
+
+while($currentValue <= $maximum) {
+    $newValue = $previousValue + $currentValue;
+
+    if ($newValue % 2 == 0) {
+        $sum += $newValue;
+    }
+
+    $previousValue = $currentValue;
+    $currentValue = $newValue;
+}
+
+echo $sum;
