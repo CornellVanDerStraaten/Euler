9d5cdfff75ceee5561bb99ddd7fdbce40792ddf7
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-01-05 23:15:36 +0100

    Gathered rules of prime factorization

--- 3/script.php
+++ 3/script.php
@@ -6,3 +6,15 @@
  * STARTED: 5 JANUARI 2022
  */
 
+// GATHERING OF RULES
+
+// If ends with 0, divide by 2
+
+// If numbers add up to something divisible by 3, then 3
+
+// If ends with 5, divisible by 5
+
+function checkIfPrimeNumber()
+{
+
+}
