a796d67821ab0f7f8f529e5dec496f35d2cf0c00
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-01-05 21:34:46 +0100

    Change to for loop and refactor

--- 1/script.php
+++ 1/script.php
@@ -9,24 +9,12 @@
  */
 
 $maximum = 1000;
-$count = 0;
-
 $sum = 0;
 
-while ($count < $maximum) {
-    $number = $count / 3;
+for($i = 0; $i < $maximum; $i++) {
+    if($i % 3 == 0) $sum += $i;
 
-    if(is_int($number)) {
-        $sum += $count;
-    }
-
-    $second_number = $count / 5;
-
-    if(is_int($second_number)) {
-        $sum += $count;
-    }
-
-    $count++;
+    if($i % 5 == 0) $sum += $i;
 }
 
 echo $sum;
