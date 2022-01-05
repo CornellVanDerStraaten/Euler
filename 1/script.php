7c3c84c3832578093038ddf55b2bda902bb70c87
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-01-05 17:40:26 +0100

    Basic example working

--- 1/script.php
+++ 1/script.php
@@ -1,3 +1,25 @@
 <?php
 
-echo 'hello world';
+$maximum = 10;
+$count = 0;
+
+$array = [];
+
+while ($count < $maximum) {
+    $number = $count / 3;
+
+    if(is_int($number)) {
+        $array[] = $count;
+    }
+
+    $second_number = $count / 5;
+
+    if(is_int($second_number)) {
+        $array[] = $count;
+    }
+
+    $count++;
+}
+
+print_r(array_sum($array));
+
