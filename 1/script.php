c9e876b67fbb6ed03a09944328aaedcf216e0924
CornellVanDerStraaten <cornellbvanders@gmail.com>
2022-01-05 17:52:17 +0100

    Refactor

--- 1/script.php
+++ 1/script.php
@@ -3,23 +3,23 @@
 $maximum = 1000;
 $count = 0;
 
-$array = [];
+$sum = 0;
 
 while ($count < $maximum) {
     $number = $count / 3;
 
     if(is_int($number)) {
-        $array[] = $count;
+        $sum += $count;
     }
 
     $second_number = $count / 5;
 
     if(is_int($second_number)) {
-        $array[] = $count;
+        $sum += $count;
     }
 
     $count++;
 }
 
-echo array_sum($array);
+echo $sum;
 
